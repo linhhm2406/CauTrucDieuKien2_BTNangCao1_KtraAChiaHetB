28ded66f0e052c9d9c9035f7fe174820962c2ce1
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:03:55 +0700

    Add file

---
+++ my.js
@@ -0,0 +1,9 @@
+let number1=parseInt(prompt('Nhap vao so a'));
+let number2=parseInt(prompt('Nhap vao so b'));
+let mod= number1%number2;
+
+if (mod==0){
+    alert('a chia het cho b');
+}else {
+    alert('a khong chia het cho b');
+}
